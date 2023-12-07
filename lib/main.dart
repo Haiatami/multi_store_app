4b7ad192812fa0c6f63f08b7e23bb17656503a3c
Haiatami <92572019+Haiatami@users.noreply.github.com>
2023-12-07 13:46:35 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:multi_store_app/auth/customer_signup.dart';
+import 'package:multi_store_app/main_screens/customer_home.dart';
+import 'package:multi_store_app/main_screens/supplier_home.dart';
+import 'package:multi_store_app/main_screens/welcome_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/welcome_screen',
+      routes: {
+        '/welcome_screen': (context) => const WelcomeScreen(),
+        '/customer_home': (context) => const CustomerHomeScreen(),
+        '/supplier_home': (context) => const SupplierHomeScreen(),
+        '/customer_signup': (context) => const CustomerRegister(),
+        // '/customer_login': (context) => const CustomerLogin(),
+        // '/supplier_signup': (context) => const SupplierRegister(),
+        // '/supplier_login': (context) => const SupplierLogin(),
+      },
+    );
+  }
+}
